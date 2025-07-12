858ab2cdc2e6790f9a16c3a760126e2dd83e9588
Omar Hossam <omarhossam5467@gmail.com>
2025-07-12 21:42:16 +0300

    Finished working on validation for Step 1 and added ValidateMessage when there's an error it will shown up globally for each step

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,8 @@
 const stepBtns = [...document.getElementsByClassName("step--btn")];
-const formSteps = [...document.getElementsByClassName("form__step")]
-const goBackBtns = [...document.getElementsByClassName("go--back--btn")]
-const nextStepBtns = [...document.getElementsByClassName("step--submit--btn")]
+const stepIds = [...document.getElementsByClassName("step--id")];
+const formSteps = [...document.getElementsByClassName("form__step")];
+const goBackBtns = [...document.getElementsByClassName("go--back--btn")];
+const nextStepBtns = [...document.getElementsByClassName("step--submit--btn")];
 
 function udpateSteps(stepId) {
   const formStep = document.getElementById(`form__step__${stepId}`);
@@ -13,7 +14,7 @@ function udpateSteps(stepId) {
   formStep.classList.add("active");
 }
 function getStepIdFromElement(el) {
-  const idStr = el.parentElement.parentElement.id;
+  const idStr = el.parentElement.parentElement.id || el.parentElement.parentElement.parentElement.id;
   return Number(idStr[idStr.length - 1]) 
 }
 stepBtns.forEach(step => {
@@ -31,9 +32,81 @@ goBackBtns.forEach(btn => {
   }
 });
 nextStepBtns.forEach(btn => {
-  btn.onclick = () => {
+  btn.onclick = (e) => {
+    e.preventDefault();
     const stepId = getStepIdFromElement(btn);
-    if (stepId === 1) return;
-    udpateSteps(stepId + 1);
+    switch(stepId) {
+      case 1:
+        validateStep1();
+        break;
+    }
   }
-})
+});
+
+function validateSuccess() {
+  return ["mediumseagreen", ""]
+} 
+function validateError(inputTitle) {
+  return ["red", `Please enter a vaild ${inputTitle}*`];
+}
+
+function validateStep1() {
+  function isValidEmail(value) {
+    let numOfAt = 0, atIndex = -1;
+    
+    value.split('').forEach(letter => numOfAt += letter === "@" ? 1 : 0);
+    console.log(numOfAt)
+    if (value.trim() === "" || numOfAt !== 1) return false;
+
+    console.log("HERE!")
+    for (let i = 0; i < value.length; i++) {
+      atIndex = value.split('')[i] === "@" ? i : -1; 
+      if (atIndex !== -1) break;
+    }
+    
+    if (atIndex !== -1) {
+      try {
+        const valueArr = value.slice(atIndex + 2, value.length).split('')
+        for (let i = 0; i < valueArr.length; i++) {
+          if (valueArr[i] === ".") {
+            if (valueArr[i + 1]) {
+              return true;
+            }
+          }
+        }
+      } catch (error) { return false; }
+    }
+    
+    return false;
+  }
+  
+  const userName = document.getElementById("user--name");
+  const userEmail = document.getElementById("user--email");
+  const userPhone = document.getElementById("user--phone");
+  
+  const validateMessage = document.querySelector("#form__step__1 .validate--message");
+  
+  if (userName.value === "") {  
+    [userName.style.outlineColor, validateMessage.textContent] = validateError("Name");
+    userName.onkeyup = () => { 
+      [userName.style.outlineColor, validateMessage.textContent] = validateSuccess()
+    }
+    return; 
+  } else { [userName.style.outlineColor, validateMessage.textContent] = validateSuccess(); }
+  
+  if (!isValidEmail(userEmail.value)) { 
+    [userEmail.style.outlineColor, validateMessage.textContent] = validateError("Email");
+    
+    userEmail.onkeyup = function() {
+      if (isValidEmail(userEmail.value)) {
+        [userEmail.style.outlineColor, validateMessage.textContent] = validateSuccess()
+      } else {
+        [userEmail.style.outlineColor, validateMessage.textContent] = validateError("Email");
+      }
+    }
+    return;
+  } else { [userEmail.style.outlineColor, validateMessage.textContent] = validateSuccess(); }
+  
+  stepIds[0].classList.add("finished");
+  udpateSteps(2);
+}
