c1499241a2a08f84097e0de36a7c7028f1f0f09d
Omar Hossam <omarhossam5467@gmail.com>
2025-07-12 20:25:02 +0300

    Finished working on Step 2 & 3, and added the functionality to the buttons except for step 1, our todo is to work on validation

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,39 @@
 const stepBtns = [...document.getElementsByClassName("step--btn")];
 const formSteps = [...document.getElementsByClassName("form__step")]
+const goBackBtns = [...document.getElementsByClassName("go--back--btn")]
+const nextStepBtns = [...document.getElementsByClassName("step--submit--btn")]
+
+function udpateSteps(stepId) {
+  const formStep = document.getElementById(`form__step__${stepId}`);
+  if (formStep.classList.contains("active")) return;
+  
+  formSteps.forEach(x => x.classList.contains("active") ? x.classList.remove("active") : "");
+  
+  void formStep.offsetWidth
+  formStep.classList.add("active");
+}
+function getStepIdFromElement(el) {
+  const idStr = el.parentElement.parentElement.id;
+  return Number(idStr[idStr.length - 1]) 
+}
 stepBtns.forEach(step => {
-  step.onclick = function() {
-    const stepId = step.firstElementChild.textContent;
-    const formStep = document.getElementById(`form__step__${stepId}`);
-    if (formStep.classList.contains("active")) return;
-    
-    formSteps.forEach(x => x.classList.contains("active") ? x.classList.remove("active") : "");
-    
-    void formStep.offsetWidth
-    formStep.classList.add("active");
+  step.onclick = () => {
+    if (step.firstElementChild.classList.contains("finished")) {
+      const stepId = step.firstElementChild.textContent;
+      udpateSteps(stepId)
+    }
+  }
+});
+goBackBtns.forEach(btn => {
+  btn.onclick = () => {
+    const stepId = getStepIdFromElement(btn);
+    udpateSteps(stepId - 1) 
+  }
+});
+nextStepBtns.forEach(btn => {
+  btn.onclick = () => {
+    const stepId = getStepIdFromElement(btn);
+    if (stepId === 1) return;
+    udpateSteps(stepId + 1);
   }
 })
-function validateForm1() {
-  
-}
